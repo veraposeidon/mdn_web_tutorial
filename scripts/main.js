ea5c9bce8a0062eaabc1faf6a75838d6bb0a8b33
SHEN Xiaohai <veraposeidon@users.noreply.github.com>
2022-07-11 23:13:02 +0800

    feat: javascript basic

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+// let myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
+
+// document.querySelector('h1').addEventListener('click', () => {
+//     alert('I am an alert box2!');
+// });
+
+let myImage = document.querySelector('img');
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png')
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName || myName === null) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla 酷毙了, ' + myName;
+    }
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storeName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了, ' + storeName;
+}
+
+myButton.onclick = function() {
+    setUserName();    
+}
